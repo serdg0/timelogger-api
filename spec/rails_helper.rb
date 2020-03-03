958fab2cf6658a332de34d73cfe36a15547b26fd
serdg0 <sergio.diazg92@gmail.com>
2020-03-02 20:01:09 -0600

    rubocop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
 
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 Shoulda::Matchers.configure do |config|
@@ -56,21 +56,21 @@ RSpec.configure do |config|
   # You can disable this behaviour by removing the line below, and instead
   # explicitly tag your specs with their type, e.g.:
   #
-# add `FactoryBot` methods
-config.include FactoryBot::Syntax::Methods
+  # add `FactoryBot` methods
+  config.include FactoryBot::Syntax::Methods
 
-# start by truncating all the tables but then use the faster transaction strategy the rest of the time.
-config.before(:suite) do
-  DatabaseCleaner.clean_with(:truncation)
-  DatabaseCleaner.strategy = :transaction
-end
-
-# start the transaction strategy as examples are run
-config.around(:each) do |example|
-  DatabaseCleaner.cleaning do
-    example.run
+  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
   end
-end
+
+  # start the transaction strategy as examples are run
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
   #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
