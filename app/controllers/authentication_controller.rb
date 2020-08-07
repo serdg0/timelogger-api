9fb950c667866f636954921b7358aa006d80c87c
Sergio Diaz <sergio.diazg92@gmail.com>
2020-08-07 17:25:26 -0500

    up and running for v2

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,6 +4,7 @@ class AuthenticationController < ApplicationController
   def authenticate
     auth_token =
       AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+    user = User.find_by(email: :email)
     json_response(auth_token: auth_token)
   end
 
