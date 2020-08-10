f3690b44ce97cd812f448f588d48bb725a8b242e
Sergio Diaz <sergio.diazg92@gmail.com>
2020-08-10 12:11:50 -0500

    test passed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
   allow do
-    origins 'https://timelogger.netlify.app', 'http://localhost:3001/'
+    origins 'https://timelogger.netlify.app'
 
     resource '/signin',
       :headers => :any,
